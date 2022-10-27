d92c980aa3e3df92f4b6b3b60408fee125013606
AKKK1 <akakichachava1@gmail.com>
2022-10-28 03:13:27 +0400

    new file:   src/components/sections/Team.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider } from "styled-components";
+
+import GlobalStyles from "./styles/GlobalStyles";
+import { light } from './styles/Themes';
+
+import Showcase from "./components/sections/Showcase";
+import Team from "./components/sections/Team";
+import Footer from "./components/Footer";
+import Navigation from "./components/Navigation";
+import Home from "./components/sections/Home";
+import Roadmap from "./components/sections/Roadmap";
+import About from "./components/sections/About";
+import Faq from "./components/sections/Faq";
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <>
+        <GlobalStyles />
+
+        <ThemeProvider theme={light} >
+          <Navigation />
+          <Home />
+          <About />
+          <Roadmap />
+          <Showcase />
+          <Team />
+          <Faq />
+          <Footer />
+
+
+
+        </ThemeProvider>
+
+      </>
     </div>
   );
 }
